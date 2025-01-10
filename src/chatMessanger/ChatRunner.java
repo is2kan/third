4fb197966d96acc87e47acff1453e2602afb1686
is2kan <lb141201@mail.ru>
2025-01-10 18:30:26 +0300

    user-registration

--- src/chatMessanger/ChatRunner.java
+++ src/chatMessanger/ChatRunner.java
@@ -1,11 +1,13 @@
 package chatMessanger;
 
+import clientPart.Registration;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ChatRunner {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите Ваше имя, которое будет использоваться в чате: " + chat.getName());
+        Registration.registration();
     }
 }
--- src/clientPart/Registration.java
+++ src/clientPart/Registration.java
@@ -1,6 +1,35 @@
 package clientPart;
 
+import chatMessanger.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Registration {
 
+    public static void registration() {
+        Scanner sc = new Scanner(System.in);
+        User user = new User();
+        System.out.print("Введите Ваш ник, которое будет использоваться в чате: ");
+        user.setName(sc.nextLine());
+        List<String> name = new ArrayList<>();
+        for (String s : name) {
+            if (!(name.contains(user.getName()))) {
+                name.add(user.getName());
+                System.out.println("Пользователь успешно зарегистрован");
+            } else {
 
+            }
+        }
+//        try {
+//            List
+//            if (map.containsKey(chat.getName())) {
+//                throw new Exception();
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Имя пользователя занято");
+//        }
+    }
 }
