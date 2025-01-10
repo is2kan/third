ef6b3df59bd6c167b83f34d08a5a9f0822b5e1d1
is2kan <lb141201@mail.ru>
2025-01-10 16:14:44 +0300

    chat-nachalo

---
+++ src/chatMessanger/Chat.java
@@ -0,0 +1,19 @@
+package chatMessanger;
+
+public class Chat {
+    private String name;
+    private String message;
+
+    public Chat(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/chatMessanger/ChatRunner.java
@@ -0,0 +1,11 @@
+package chatMessanger;
+
+import java.util.Scanner;
+
+public class ChatRunner {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите Ваше имя, которое будет использоваться в чате: " + chat.getName());
+    }
+}
