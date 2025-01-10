36292932ec46adda838ad09a674ecc23009d323a
Bogdan Berkun <berkun508@gmail.com>
2025-01-10 17:20:48 +0500

    I love

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
-        if (age < 18) {
-            System.out.println("Ты старый");
+        if (age > 18 && age < 65) {
+            System.out.println("Ты в самом соку");
         }
+
     }
 }
